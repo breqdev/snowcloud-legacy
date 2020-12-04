7b8d145d93032b339bd67af26bebc327859d2b16
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-12-03 22:19:52 -0500

    Add the SnowCloud server and a simple client

---
+++ app.py
@@ -0,0 +1,66 @@
+import os
+import time
+
+import redis
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+db = redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
+
+
+SNOWCLOUD_KEY = os.getenv("SNOWCLOUD_KEY")
+
+TIME_TO_LIVE = 60
+
+if not db.exists("snowcloud:id:pool"):
+    for i in range(2**10):
+        db.zadd("snowcloud:id:pool", {i: 0})
+
+
+@app.route("/", methods=["POST"])
+def index():
+    user = request.args.get("user")
+    key = request.args.get("key")
+
+    if not user:
+        return abort(403)
+    if not key:
+        return abort(403)
+
+    if key != SNOWCLOUD_KEY:
+        return abort(403)
+
+    renew = request.args.get("renew")
+
+    if renew:
+        last_expires = db.zscore("snowcloud:id:pool", renew)
+        if not last_expires:
+            return abort(403)  # invalid ID
+
+        last_user = db.get(f"snowcloud:id:user:{renew}")
+        if last_user != user:
+            return abort(403)  # Not used by the same user
+
+        worker_id = renew
+
+    else:
+        worker_id = db.zrange("snowcloud:id:pool", 0, 0)[0]
+
+        last_expires = db.zscore("snowcloud:id:pool", worker_id)
+        if float(last_expires) > time.time():
+            return abort(500)  # no IDs available
+
+        db.set(f"snowcloud:id:user:{worker_id}", user)
+
+    expires = time.time() + TIME_TO_LIVE
+    db.zadd("snowcloud:id:pool", {worker_id: expires})
+
+    return jsonify({
+        "worker_id": worker_id,
+        "expires": expires,
+        "ttl": TIME_TO_LIVE
+    })
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
